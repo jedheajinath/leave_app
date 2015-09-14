cc1e4ee380310425c2410851f7ace5a6ffd122c3
jedheajinath <ajinath.jedhe@wwindia.com>
2015-09-14 20:05:11 +0530

    Server side validation with valid Date format

--- app/models/leave.rb
+++ app/models/leave.rb
@@ -2,7 +2,7 @@ class Leave < ActiveRecord::Base
   belongs_to :user
   has_one :status, dependent: :destroy
 
-  # validates :from_date, :to_date, :reason,  presence: true
+  validates :from_date, :to_date, :reason,  presence: true
   validate :from_date_cannot_be_in_the_past
   validate :to_date_cannot_be_less_than_from_date
 
@@ -15,15 +15,36 @@ class Leave < ActiveRecord::Base
 
   private
     def from_date_cannot_be_in_the_past
-      if from_date < Date.today + 1
-        errors.add(:from_date, "Can't in  Past or Todays Date")
+      if !from_date.nil?
+        self.errors.clear
+        errors.add(:from_date, "Can't in  Past or Todays Date") if
+        from_date < Date.today + 1
+      elsif valid_date?(from_date)
+        self.errors.clear
+        errors.add(:from_date, "Date must in following Format dd/mm/yyyy")
       end
     end
 
     def to_date_cannot_be_less_than_from_date
-      if to_date <= from_date || (to_date - from_date).to_i > 30
+      if valid_date?(to_date) || valid_date?(from_date)
+        errors.add(:to_date, "Date must in following Format dd/mm/yyyy")
+      elsif !to_date.nil?
         errors.add(:to_date, "Must Greater than From date or
-         Leave cant more than 30 days")
+        Leave cant more than 30 days") if
+        to_date <= from_date || (to_date - from_date).to_i > 30
       end
     end
+
+    def valid_date?(date,format = "%d/%m/%Y")
+      begin
+        Date.strptime(date,format)
+        return false
+      rescue
+        return true
+      end
+    end
+
 end
+
+
+
