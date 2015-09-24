ea39963cc31bc78499ba153f7ec9b062ec5a3b5c
jedheajinath <ajinath.jedhe@wwindia.com>
2015-09-24 16:29:55 +0530

    log out

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module LeaveManagement
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.serve_static_assets = true
     config.active_record.raise_in_transactional_callbacks = true
     config.beginning_of_week = :sunday
   end
